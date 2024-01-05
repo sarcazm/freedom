170699264c26c3b52426e70bd3e145d3e298042c
Nikolay <Nikol.Marchenko@x5.ru>
2024-01-06 01:58:16 +0300

    init

---
+++ src/test/java/com/freedom/hello/FreedomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freedom.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreedomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
